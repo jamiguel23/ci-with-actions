8dbf672e0bb4c2ba34e813100c4253f3d07a717a
Matt Miguel <josematthewmiguel@gmail.com>
2023-02-21 19:30:24 +0000

    added routes

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
 var http = require("http");
 http.createServer(function(req,res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Aloha world');
+
+  var path = req.url.toLowerCase();
+
+  switch(path){
+
+    case "/": 
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      res.end('home hit');
+      break;
+
+    case "/about": 
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      res.end('about page hit');
+      break; 
+
+    default:
+      res.writeHead(404, {'Content-Type': 'text/plain'});
+      res.end('nothing page');
+      
+  }
+
 }).listen(process.env.PORT || 3000);
